6c8c1588559207963862e1f4868079d6b30ecf8f
winesht <vvolk0kuzn@gmail.com>
2026-04-21 16:12:57 +0200

    added weight , height, age, gender

--- akarpenko_php-master/index.php
+++ akarpenko_php-master/index.php
@@ -4,3 +4,24 @@ function getBMI($weight, $height) { // waga i wzrost w cm
     $h = $height / 100;
     return round($weight / ($h * $h), 1);
 }
+
+function getBMILabel($bmi) {
+    if ($bmi < 18.5) {
+        return 'niedowaga';
+    } elseif ($bmi < 25) {
+        return 'waga prawidłowa';
+    } elseif ($bmi < 30) {
+        return 'nadwaga';
+    } else {
+        return 'otyłość';
+    }
+}
+
+function getBMR($w, $h, $a, $g) { // waga, wzrost, wiek, płeć
+    $base = (10 * $w) + (6.25 * $h) - (5 * $a);
+    if ($g === 'm') {
+        return round($base + 5);
+    } else {
+        return round($base - 161);
+    }
+}
